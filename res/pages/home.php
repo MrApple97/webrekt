e9860aec12f6cbf606643dc826835b21b8fdc773
mordekaiseruno <dominikdavid@Angela-Schwaabs-MacBook-Pro.local>
2014-12-23 20:59:36 +0100

    removed obsolete config requiring

--- res/pages/home.php
+++ res/pages/home.php
@@ -1,13 +1,9 @@
-<?php
-	require_once "../res/config.php";
-?>
-
 <head>
 <title>HOE</title>
 <link rel="stylesheet" type="text/css" href=<?php echo "\"$MAIN_STYLE\""?>>
 </head>
 <body>
 <?php
-	echo "fickificki";
+	echo "Hallo";
 ?>
 </body>
