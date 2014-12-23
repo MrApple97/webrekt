095b8b91f1d2ac24c0cec15bc119d0ac4cf3ab6d
mordekaiseruno <dominikdavid@Angela-Schwaabs-MacBook-Pro.local>
2014-12-23 22:39:57 +0100

    added anchors

--- res/pages/services.php
+++ res/pages/services.php
@@ -10,18 +10,18 @@
 	<p>
 		<h1>Unsere Leistungen</h1>
 		<ul>
-			<li>Web & Programming</li>
-			<li>Video & Grafik</li>
-			<li>Sound & Musik</li>
+			<a href="#Web_Programming"><li>Web & Programming</li></a>
+			<a href="#Video_Grafik"><li>Video & Grafik</li></a>
+			<a href="#Sound_Musik"><li>Sound & Musik</li></a>
 		</ul>
 		<br><br>
-		<h2>Web & Programming</h2>
+		<a name="Web_Programming" href="#Web_Programming"><h2>Web & Programming</h2></a>
 		Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
 		<br><br>
-		<h2>Video & Grafik</h2>
+		<a name="Video_Grafik" href="#Video_Grafik"><h2>Video & Grafik</h2></a>
 		Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
 		<br><br>
-		<h2>Sound & Musik</h2>
+		<a name="Sound_Musik" href="#Sound_Musik"><h2>Sound & Musik</h2></a>
 		Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
 	</p>
 </div>
